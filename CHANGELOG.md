f2cd34b1b5a1e58920a1c70d2e32a9b8c88ce0df
Adam Leff <adamleff@chef.io>
2016-01-22 09:36:25 -0500

    version bump to 0.3.0 and adding changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# redhat_subscription_manager cookbook
+
+## v0.3.0 (2016-01-22)
+* [pr#6](https://github.com/chef-partners/redhat-subscription-manager-cookbook/pull/6) Install yum-plugin-security on RHEL6 to support the rhsm_errata_level resource
+
+## v0.2.0 (2015-12-21)
+* [pr#4](https://github.com/chef-partners/redhat-subscription-manager-cookbook/pull/4) Make installation of the katello agent optional
+
+## v0.1.0 (2015-11-24)
+* Initial release
+
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'partnereng@chef.io'
 license 'Apache 2.0'
 description 'Provides custom resources to interact with Red Hat Subscription Manager (RHSM) and Red Hat Satellite'
 long_description 'Provides custom resources to interact with Red Hat Subscription Manager (RHSM) and Red Hat Satellite'
-version '0.2.0'
+version '0.3.0'
 source_url 'https://github.com/chef-partners/redhat-subscription-manager-cookbook' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-partners/redhat-subscription-manager-cookbook/issues' if respond_to?(:issues_url)
 
