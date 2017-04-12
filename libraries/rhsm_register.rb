ebc943c016c19ff9ad9f84c8d1a960ceadba5e22
Dylan Myers <dylan.myers@bluemedora.com>
2017-04-12 08:55:39 -0400

    Fix missing not_if checks to prevent things from being run when they shouldn't

--- libraries/rhsm_register.rb
+++ libraries/rhsm_register.rb
@@ -35,7 +35,7 @@ module RhsmCookbook
 
     action :register do
       remote_file "#{Chef::Config[:file_cache_path]}/katello-package.rpm" do
-        source "http://#{satellite_host}/pub/katello-ca-consumer-latest.noarch.rpm"
+        source "https://#{satellite_host}/pub/katello-ca-consumer-latest.noarch.rpm"
         action :create
         notifies :install, 'yum_package[katello-ca-consumer-latest]', :immediately
         not_if { satellite_host.nil? || registered_with_rhsm? || katello_cert_rpm_installed? }
@@ -45,10 +45,12 @@ module RhsmCookbook
         options '--nogpgcheck'
         source "#{Chef::Config[:file_cache_path]}/katello-package.rpm"
         action :nothing
+        not_if { satellite_host.nil? || registered_with_rhsm? || katello_cert_rpm_installed? }
       end
 
       file "#{Chef::Config[:file_cache_path]}/katello-package.rpm" do
         action :delete
+        only_if { ::File.exist?("#{Chef::Config[:file_cache_path]}/katello-package.rpm") }
       end
 
       execute 'Register to RHSM' do # ~FC009
